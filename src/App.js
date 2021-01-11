9dd6f34b86ee1209bae30ad2adb3ab78946ddc1b
Curtis <chender93@gmail.com>
2021-01-10 22:29:19 -0500

    Updated rendering functions

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About_Resume';
 import Projects from './components/Projects';
@@ -6,14 +7,31 @@ import Contact from './components/Contact';
 import './App.css';
 
 function App() {
+  const [currentSection, setCurrentSection] = useState('About');
+
+  const renderPage = () => {
+    switch (currentSection) {
+      case 'Projects':
+        return <Projects />;
+      case 'Contact':
+        return <Contact />;
+      default:
+        return <About />
+    }
+  }
   return (
     <div>
-      <Nav></Nav>
-      <About></About>
-      <Contact></Contact>
-      <Projects></Projects>
-      <Footer></Footer>
-    </div>
+        <Nav
+        currentSection={currentSection}
+        setCurrentSection={setCurrentSection}
+        ></Nav>   
+        
+        <main>
+            {renderPage(currentSection)}
+        </main>
+        
+        <Footer></Footer>
+    </div>  
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Nav() {
+function Nav(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
@@ -11,13 +11,16 @@ function Nav() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav text-info">
               <li className="nav-item">
-                <a data-testid="about" className="nav-link active text-info" aria-current="page" href="#about">ABOUT</a>
+                <a data-testid="about" className="nav-link active text-info" aria-current="page" href="#about" 
+                onClick={() => props.setCurrentSection('About')}>ABOUT</a>
               </li>
               <li className="nav-item">
-                <a data-testid="projects" className="nav-link active text-primary" href="#projects">PPROJECTS</a>
+                <a data-testid="projects" className="nav-link active text-primary" href="#projects"
+                onClick={() => props.setCurrentSection('Projects')}>PPROJECTS</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active text-info" href="#contact">CONTACT</a>
+                <a className="nav-link active text-info" href="#contact"
+                onClick={() => props.setCurrentSection('Contact')}>CONTACT</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link active text-primary" href="https://docs.google.com/document/d/1KEJ08JK2DL0Dp5sxDnwiw0E1P_7AOnN5s_D6BJ81Dvc/edit?usp=sharing">RESUME</a>
